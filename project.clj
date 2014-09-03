d6087aa923d1b718b1d36ad027ba614d076463d5
Joel Friedman <asher.friedman@gmail.com>
2014-09-03 11:46:57 -0500

    0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject joelash/twilio-api "0.1.1"
+(defproject joelash/twilio-api "0.1.2-SNAPSHOT"
   :description "Clojure wrapper for Twillio"
   :url "http://owainlewis.com"
   :license {:name "Eclipse Public License"
